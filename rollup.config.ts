cdfe260fb7619cf76674fa41b1fb73a750664c9e
Anand Chowdhary <anandchowdhary@gmail.com>
2019-02-13 14:51:31 +0100

    Add UMD and ES

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,19 +1,28 @@
 import typescript from "rollup-plugin-typescript2";
 import sourceMaps from "rollup-plugin-sourcemaps";
+import resolve from "rollup-plugin-node-resolve";
 
 const pkg = require("./package.json");
 
 export default {
   input: `src/index.ts`,
-  output: {
-    file: pkg.main,
-    name: pkg.module,
-    format: "es",
-    sourcemap: true
-  },
-  external: [],
+  output: [
+    {
+      file: pkg.main,
+      name: pkg.main,
+      format: "umd"
+    },
+    {
+      file: pkg.module,
+      format: "es"
+    }
+  ],
+  external: [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {})
+  ],
   watch: {
     include: "src/**"
   },
-  plugins: [typescript(), sourceMaps()]
+  plugins: [typescript(), resolve(), sourceMaps()]
 };
